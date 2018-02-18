280b6af8ba0ec12f1e398668cee14e45c17fb473
katherineRamirez <oeillet8_5@hotmail.com>
2018-02-18 13:35:47 -0300

    agregandno detalles

---
+++ assets/js/jsSeleccion.js
@@ -0,0 +1,11 @@
+let selection = 
+
+objeto_datos = new Object();
+objeto_datos.titulo = titulo;
+
+let objeto_serializado = JSON.stringify(objeto_datos);
+
+
+window.localStorage.setItem('objdatos', objeto_serializado);
+
+
