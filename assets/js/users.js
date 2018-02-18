c2d5a95ff530ba40fdab7e655aafe3228d65f5a3
Claudina Cortez de Leon <claudinacr@gmail.com>
2018-02-18 15:17:32 -0300

    Autentiicación

--- assets/js/users.js
+++ assets/js/users.js
@@ -2,19 +2,46 @@
     let password = document.getElementById('password');
     let button = document.getElementById('button');
 
-    button.addEventListener('click', e => {
+button.addEventListener('click', e => {
+        
+    if (firebase.auth().currentUser) {
+    
+        firebase.auth().signOut();
+        
+    }else{
         //Obtener email y password
         const userEmail = email.value;
         const passwordUser = password.value;
-        console.log(userEmail);
+        // console.log(userEmail);
         const auth = firebase.auth();
-        const promise = auth.signInWithEmailAndPassword(userEmail, passwordUser);
+        const promise = auth.signInWithEmailAndPassword(userEmail, passwordUser).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // [START_EXCLUDE]
+            if (errorCode === 'auth/wrong-password') {
+              alert('Wrong password.');
+            } else {
+              alert(errorMessage);
+            }
+            console.log(error);
+            
+            //document.getElementById('quickstart-sign-in').disabled = false;
+            // [END_EXCLUDE]
+        });
         
+        auth.onAuthStateChanged(function (user) {
+            if (user) {
+                console.log('si');
+                window.location = "index.html";
+            }
+        });
+    }
+    
+});
 
-    });
 
-
-    let logout = document.getElementById('logout');
+let logout = document.getElementById('logout');
 
 
     logout.addEventListener('click', e => {
@@ -22,10 +49,10 @@
         firebase.auth().signOut();
     });
 
-    firebase.auth().onAuthStateChanged(firebaseUser =>{
-        if(firebaseUser){
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if (firebaseUser) {
             logout.classList.remove('hide');
-        }else{
+        } else {
             logout.classList.add('hide');
         }
     })
