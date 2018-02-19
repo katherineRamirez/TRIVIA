580493c36ec1faa7018be4bf250934d93e2e06eb
Claudina Cortez de Leon <claudinacr@gmail.com>
2018-02-19 09:25:38 -0300

    Auto stash before merge of "master" and "katherineRamirez/master"

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,31 +3,40 @@ let objSerialized = window.localStorage.getItem('objdatos');
 obj = JSON.parse(objSerialized);
 selectionCalled = obj.selectionCall;
 
-
+let btnStart = document.getElementById('btnStart');
+if (selectionCalled === 'boolean') {
+  btnStart.addEventListener('click', function() {
+    btnStart.setAttribute('href', 'questions.html');
+  })
+} else {
+  btnStart.addEventListener('click', function() {
+    btnStart.setAttribute('href', 'booleanQuestions.html');
+  })
+}
 fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=' + selectionCalled)
 
-  .then(function(response) {
+  .then(function (response) {
     // Turns the the JSON into a JS object
     return response.json();
   })
 
-  .then(function(data) {
+  .then(function (data) {
     console.log(data);
 
     let type = data.results[0].type;
     //console.log(type)
 
-  let btnTrue = document.getElementById('true'); // botón verdadero
-  let btnFalse = document.getElementById('false'); // botón falso
-  let correctAnswer = data.results[0].correct_answer;
-  console.log(correctAnswer)
-  let incorrectAnswer = data.results[0].incorrect_answer;
+    let btnTrue = document.getElementById('true'); // botón verdadero
+    let btnFalse = document.getElementById('false'); // botón falso
+    let correctAnswer = data.results[0].correct_answer;
+    console.log(correctAnswer)
+    let incorrectAnswer = data.results[0].incorrect_answer;
 
-/* Creando container de preguntas de verdadero y falso */
-  $('#containerQuestionsTrueFalse').append('<div class="container contQuestion"><div class="row"><p><div class="col s10 push-s1 pull-s1 textQuestion">'
-  	+ data.results[0].question + '</p></div></div></div>'+
-  	'<div class="container contQuestion"><div class="row"><div class="col s6"><a href="#" id="true" class="waves-effect waves-light btn">Verdadero</a></div>'+
-  	'<div class="col s6"><a href="#" id="false" class="waves-effect waves-light btn">Falso</a></div></div></div>')
+    /* Creando container de preguntas de verdadero y falso */
+    $('#containerQuestionsTrueFalse').append('<div class="container contQuestion"><div class="row"><p><div class="col s10 push-s1 pull-s1 textQuestion">' +
+      data.results[0].question + '</p></div></div></div>' +
+      '<div class="container contQuestion"><div class="row"><div class="col s6"><a href="#" id="true" class="waves-effect waves-light btn">Verdadero</a></div>' +
+      '<div class="col s6"><a href="#" id="false" class="waves-effect waves-light btn">Falso</a></div></div></div>')
 
   $('#true').click(function(){
     if(data.results[0].correct_answer == 'True'){
@@ -47,10 +56,10 @@ fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=' +
 
   });
 
-  /* Creando container de preguntas de opción múltiple 
-  $('#containerQuestionsMultipleChoice').append('<div class="container contQuestionMultiple"><div class="row"><div class="col s12">'+
-    data.results[0].question + '</div></div><div class="row"><div class="col s12><p>'+
-    data.results[0].incorrect_answer[0]+'</p></div></div><div class="row"></div><div class="col s12">'+
-    '</div><p>'+ data.results[0].incorrect_answer[1]+'</p></div></div><div class="row"></div><div class="col s12">'+
-    '</div><p>'+ data.results[0].correct_answer+'</p></div></div><div class="row"></div><div class="col s12">'+
-    '</div><p>'+ data.results[0].incorrect_answer[2]+'</p></div>')*/
+/* Creando container de preguntas de opción múltiple 
+$('#containerQuestionsMultipleChoice').append('<div class="container contQuestionMultiple"><div class="row"><div class="col s12">'+
+  data.results[0].question + '</div></div><div class="row"><div class="col s12><p>'+
+  data.results[0].incorrect_answer[0]+'</p></div></div><div class="row"></div><div class="col s12">'+
+  '</div><p>'+ data.results[0].incorrect_answer[1]+'</p></div></div><div class="row"></div><div class="col s12">'+
+  '</div><p>'+ data.results[0].correct_answer+'</p></div></div><div class="row"></div><div class="col s12">'+
+  '</div><p>'+ data.results[0].incorrect_answer[2]+'</p></div>')*/
