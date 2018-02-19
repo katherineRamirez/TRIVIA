86b572a4de81e01c6f88571b59498a7a4c0e0e4a
katherineRamirez <oeillet8_5@hotmail.com>
2018-02-19 08:51:37 -0300

    intento por relacionar respuesta correcta

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,26 +13,44 @@ fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=' +
 
   .then(function(data) {
     console.log(data);
-    
+
+    let type = data.results[0].type;
+    //console.log(type)
+
   let btnTrue = document.getElementById('true'); // botón verdadero
   let btnFalse = document.getElementById('false'); // botón falso
   let correctAnswer = data.results[0].correct_answer;
-  //console.log(correctAnswer)
+  console.log(correctAnswer)
   let incorrectAnswer = data.results[0].incorrect_answer;
 
 /* Creando container de preguntas de verdadero y falso */
-  $('#containerQuestions').append('<div class="container contQuestion"><div class="row"><p><div class="col s10 push-s1 pull-s1 textQuestion">'
+  $('#containerQuestionsTrueFalse').append('<div class="container contQuestion"><div class="row"><p><div class="col s10 push-s1 pull-s1 textQuestion">'
   	+ data.results[0].question + '</p></div></div></div>'+
-  	'<div class="container contQuestion"><div class="row"><div class="col s6"><a href="correct.html" id="true" class="waves-effect waves-light btn">Verdadero</a></div>'+
-  	'<div class="col s6"><a href="incorrect.html" id="false" class="waves-effect waves-light btn">Falso</a></div></div></div>')
+  	'<div class="container contQuestion"><div class="row"><div class="col s6"><a href="#" id="true" class="waves-effect waves-light btn">Verdadero</a></div>'+
+  	'<div class="col s6"><a href="#" id="false" class="waves-effect waves-light btn">Falso</a></div></div></div>')
 
-  /* Creando container de preguntas de opción múltiple */
+  $('#true').click(function(){
+    if(data.results[0].correct_answer === data.results[0].correct_answer){
+      $('#true').attr('href', 'correct.html');
+    }else if(data.results[0].correct_answer === data.results[0].incorrect_answer[0]){
+      $('#true').attr('href', 'incorrect.html');
+    }
+  })
+
+  $('#false').click(function(){
+    if(data.results[0].correct_answer === data.results[0].correct_answer){
+      $('#false').attr('href', 'correct.html');
+    }else if(data.results[0].correct_answer === data.results[0].incorrect_answer[0]){
+      $('#false').attr('href', 'incorrect.html');
+    }
+  })
+
+  });
+
+  /* Creando container de preguntas de opción múltiple 
   $('#containerQuestionsMultipleChoice').append('<div class="container contQuestionMultiple"><div class="row"><div class="col s12">'+
     data.results[0].question + '</div></div><div class="row"><div class="col s12><p>'+
     data.results[0].incorrect_answer[0]+'</p></div></div><div class="row"></div><div class="col s12">'+
     '</div><p>'+ data.results[0].incorrect_answer[1]+'</p></div></div><div class="row"></div><div class="col s12">'+
     '</div><p>'+ data.results[0].correct_answer+'</p></div></div><div class="row"></div><div class="col s12">'+
-    '</div><p>'+ data.results[0].incorrect_answer[2]+'</p></div>')
-
-  });
-
+    '</div><p>'+ data.results[0].incorrect_answer[2]+'</p></div>')*/
