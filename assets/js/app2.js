b6a41922c09ba5286ace134829069279132aa383
katherineRamirez <oeillet8_5@hotmail.com>
2018-02-19 13:56:03 -0300

    cambios en app2.js

--- assets/js/app2.js
+++ assets/js/app2.js
@@ -13,23 +13,35 @@ fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=' +
   .then(function (data) {
     console.log(data);
 
-    let opt1 = document.getElementById('opt1');
-    let opt2 = document.getElementById('opt2');
-    let opt3 = document.getElementById('opt3');
-    let opt4 = document.getElementById('opt4');
+    let opt1 = data.results[0].incorrect_answers[0];
+    let opt2 = data.results[0].incorrect_answers[1];
+    let opt3 = data.results[0].correct_answer;
+    let opt4 = data.results[0].incorrect_answers[2];
+
+    let arrIncorrect = [];
+    
 
     for(let j = 0; j < data.results[0].incorrect_answers[0].length; j++){
       //console.log(data.results[0].incorrect_answers[j])
       let incorrectAnswers = data.results[0].incorrect_answers[j];
-  
+      //console.log(incorrectAnswers)
+      arrIncorrect.push(incorrectAnswers);  
+    }
+    //console.log(arrIncorrect);
+
+    for(let k = 0; k < arrIncorrect[0].length; k++){
+      //console.log(data.results[0].incorrect_answers[j])
+      let arr = arrIncorrect[k];
+      //console.log(arr);
+
     }
 
     /*Creando container de preguntas de opción múltiple */
 $('#containerQuestionsMultipleChoice').append('<div class="row"><div class="col s11 offset-s1"><h3 class="titleMultiple">'+data.results[0].question+'</h3></div></div><div class="row">'+
-  '<div class="col s11 offset-s1 opt"><p class="options"><a id="opt1" href="#">'+data.results[0].incorrect_answers[0]+'</a></p></div></div><div class="row"><div class="col s11 offset-s1 opt">'+
-  '<p class="options"><a id="opt2" href="#">'+data.results[0].incorrect_answers[1]+'</a></p></div></div><div class="row"><div class="col s11 offset-s1 opt"><p class="options"><a id="opt3" href="#">'+
-  data.results[0].correct_answer+'</a></p></div></div><div class="row"><div class="col s11 offset-s1 opt"><p class="options"><a id="opt4" href="#">'+
-  data.results[0].incorrect_answers[2]+'</a></p></div></div></div>')
+  '<div class="col s11 offset-s1 opt"><a id="opt1" href="#"><p class="options">'+data.results[0].incorrect_answers[0]+'</p></a></div></div><div class="row"><div class="col s11 offset-s1 opt">'+
+  '<a id="opt2" href="#"><p class="options">'+data.results[0].incorrect_answers[1]+'</p></a></div></div><div class="row"><div class="col s11 offset-s1 opt"><a id="opt3" href="#"><p class="options">'+
+  data.results[0].correct_answer+'</p></a></div></div><div class="row"><div class="col s11 offset-s1 opt"><a id="opt4" href="#"><p class="options">'+
+  data.results[0].incorrect_answers[2]+'</p></a></div></div></div>')
 
 
 });
